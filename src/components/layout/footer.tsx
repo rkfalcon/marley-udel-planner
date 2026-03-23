d97e0118e474d04ee70c73549aea342785e29df6
rkfalcon <rkfalcon@gmail.com>
2026-03-23 18:46:17 -0400

    feat: update

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,11 @@
+export function Footer() {
+  return (
+    <footer className="border-t bg-[#00539F] mt-auto">
+      <div className="max-w-7xl mx-auto px-4 py-4 flex items-center justify-center">
+        <p className="text-blue-100 text-sm text-center">
+          Built for Marley Falcon&nbsp;&nbsp;|&nbsp;&nbsp;University of Delaware&nbsp;&nbsp;|&nbsp;&nbsp;Cognitive Science BS &mdash; PPSLP
+        </p>
+      </div>
+    </footer>
+  );
+}
